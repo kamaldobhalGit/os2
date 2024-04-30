61c733cbf3b9d2cc42be775890911fa48507e558
kamalDobhalGit <kamaldobhal1995@gmail.com>
2024-04-30 23:39:27 +0530

    added browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import TopNav from "./components/topNav/topNav";
 import Tabs from "./components/tabs/tabs";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <TopNav />
-      <Tabs />
+      <Router>
+        <Routes>
+          <Route path="/" Component={Tabs} />
+        </Routes>
+      </Router>
     </>
   );
 }
